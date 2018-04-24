5f10eef50265d4abf6ebaa80e64b9351dbc4aedc
marizaswart <mariza.swart@gmail.com>
2018-04-24 10:27:03 +0200

    Add template and blurb for team page

--- src/js/cms-preview-templates/team.js
+++ src/js/cms-preview-templates/team.js
@@ -3,22 +3,7 @@ import { List } from 'immutable';
 
 import Jumbotron from "./components/jumbotron";
 
-const MediaBlock = ({heading, text, imageUrl, reverse}) => {
-  const imageContainerClassName = reverse
-    ? "ph3-m w-50-m"
-    : "ph3-m w-50-m order-last-m";
-  return <div className="flex-m mhn3-m mb4">
-    <div className={imageContainerClassName}>
-      <img src={imageUrl} alt="" className="db mb2" />
-    </div>
-    <div className="ph3-m w-50-m">
-      <h3 className="f3 b lh-title mb1">{heading}</h3>
-      <p>{text}</p>
-    </div>
-  </div>;
-};
-
-export default class ValuesPreview extends React.Component {
+export default class TeamPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
 
@@ -37,22 +22,14 @@ export default class ValuesPreview extends React.Component {
 
       <div className="bg-off-white pv4">
         <div className="ph3 mw7 center">
-          <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "intro", "heading"])}</h2>
 
           <div className="flex-ns flex-wrap mhn2-ns mb3">
             {(entry.getIn(["data", "intro", "blurbs"]) || []).map((blurb, index) => <div className="ph2-ns w-50-ns mb4" key={index}>
               <img src={blurb.get("image") && getAsset(blurb.get("image"))} alt="" className="center db mb3" style={{width: "240px"}}/>
+              <p>{blurb.get("heading")}</p>
               <p>{blurb.get("text")}</p>
             </div>)}
           </div>
-        </div>
-      </div>
-
-      <div className="bg-off-white pv4">
-        <div className="mw7 center ph3 pt4">
-          {values.map(({text, heading, imageUrl}, i) =>
-            <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
-          )}
         </div>
       </div>
     </div>;
