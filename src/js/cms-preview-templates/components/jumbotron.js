7b9321c16baf1c67bf1999e2e231ab34038a204a
marizaswart <mariza.swart@gmail.com>
2018-04-25 10:35:03 +0200

    Style cms jumbotron

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -4,18 +4,18 @@ export default class Jumbotron extends React.Component {
   render() {
     const {image, title, subtitle} = this.props;
     return <div>
-      <div className="pv5 pv6-l ph3 bg-center cover" style={{
+      <div className="pv5 pv6-l ph3 bg-center cover hero" style={{
         backgroundImage: image && `url(${image})`
       }}>
         <div className="mw7 center ph3">
-          <div className="db mb3">
-            <div className="mw7 relative bg-fix-primary mb3">
-              <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
+          <div className="db center mb3">
+            <div className="mw7 relative mb3">
+              <h1 className="f2 f1-l b di lh-title mb3 white mw6">
                 { title }
               </h1>
             </div>
-            <div className="mw7 relative bg-fix-primary">
-              {subtitle && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{ subtitle }</p>}
+            <div className="mw7 relative">
+              {subtitle && <p className="b f4 di lh-title mb3 white mw6">{ subtitle }</p>}
             </div>
           </div>
         </div>
