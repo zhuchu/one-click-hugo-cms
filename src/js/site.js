026365d20598942783295354075c076952211e16
marizaswart <mariza.swart@gmail.com>
2018-05-07 13:57:55 +0200

    WIP chatbot buttons

--- src/js/site.js
+++ src/js/site.js
@@ -1,23 +1,19 @@
 import ApiAi from "./api"
 
-function goBack() {
-  document.history.back();
-}
-
-const sourceSwap = function() {
-  const $this = $(this);
-  const newSource = $this.data("alt-src");
-  $this.data("alt-src", $this.attr("src"));
-  $this.attr("src", newSource);
-}
-
-$(function() {
-  $("img[data-alt-src]").each(function() {
-    new Image().src = $(this).data("alt-src");
-  }).hover(sourceSwap, sourceSwap);
-});
-
 $(document).ready(function() {
+  const sourceSwap = function() {
+    const $this = $(this);
+    const newSource = $this.data("alt-src");
+    $this.data("alt-src", $this.attr("src"));
+    $this.attr("src", newSource);
+  }
+
+  $(function() {
+    $("img[data-alt-src]").each(function() {
+      new Image().src = $(this).data("alt-src");
+    }).hover(sourceSwap, sourceSwap);
+  });
+
   $(".show_hide").click(function() {
     $(".slidein").toggleClass("transition");
   });
@@ -27,7 +23,20 @@ $(document).ready(function() {
     let n = $("#section").height();
     $("html, body").animate({scrollTop: n}, 800);
   });
+
+  $("#select-option").click(function() {
+    chatSelectButton();
+  });
+
+  $(".close-icon").click(function() {
+    goBack();
+  });
+
 });
+
+function goBack() {
+  document.history.back();
+}
 
 const client = new ApiAi.ApiAiClient({accessToken: '9e8c9e20ba51493b81dd71fe81153214'})
 .textRequest('test')
@@ -39,3 +48,19 @@ const client = new ApiAi.ApiAiClient({accessToken: '9e8c9e20ba51493b81dd71fe8115
     debugger;
 
   })
+
+function chatSelectButton(button) {
+  debugger;
+  const buttonSelections = document.getElementById("select-option").innerText;
+  console.log(button.value);
+  let i = 0;
+  for (i = 0; i < buttonSelections.length; i++) {
+    const clickedButton = buttonSelections[i];
+    let conversationAnswer = "";
+    if (clickedButton.selected === true) {
+      return conversationAnswer = "<p>Hi," + clickedButton.textRequest(value) + "</p>"
+    }
+  }
+  let output = document.getElementById("output");
+  output.innerHTML = conversationAnswer;
+}
